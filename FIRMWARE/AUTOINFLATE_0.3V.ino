43b6e6a50fd9b12c6059bdddd4bd9a5dd16ca711
Tevian <104283546+MagicPhase@users.noreply.github.com>
2023-11-18 05:46:12 -0800

    Update AUTOINFLATE_0.3V.ino

--- FIRMWARE/AUTOINFLATE_0.3V.ino
+++ FIRMWARE/AUTOINFLATE_0.3V.ino
@@ -9,12 +9,12 @@
 AUTOINFLATE V0.3 - 11/18/23
 BY Tevian Busselle 
 
-All variables including pressures, settings, and times are converted from the (encoderInput) counter. This has the effect of limting the divisions of certain
+All variables including pressures, settings, and times are converted from the (encoderInput) counter. This has the effect of limiting the divisions of certain
 variables like pressure and pump speeds. For instance, while the pump PWM has a value from 0-1023, this would require the encoder to be turned too many times
-to acheieve the max value. The division of the PWM speed is dictated by the number of pixels in the display graph. This is handled via the map function to
+to achieve the max value. The division of the PWM speed is dictated by the number of pixels in the display graph. This is handled via the map function to
 convert 0-1023 steps into 51 pixel steps. The mapped number is stored for use while the displayed value/graph is converted from that stored number. 
 
-While the displayed pressures are in PSI, the stored pressure vaules are in (mBar * 100). Example: The default stored value for the maximum "selectable" 
+While the displayed pressures are in PSI, the stored pressure values are in (mBar * 100). Example: The default stored value for the maximum "selectable" 
 pressure from the interface is (13788 / 100 = 2PSI) and is stored in (airSys.maxPressure). The ABSOLUTE MAXIMUM SYSTEM PRESSURE is set by limiting the 
 encoder input via the (MaxPressure) variable while in the AIRSYS menu.
 
@@ -25,14 +25,14 @@ byte MaxPressure = 41;//This number should remain low! Somewhere around 30 for 3
 Default system variables like pressures and PWM are stored in "storedData" function with the relevant variables. These defaults are initiated by the preference function.
 
 Important interface notes:
-Inputing pressures over the selected maximum will reduce the value to the maximum. An inflation task "hug or profile" will only execute if the times are not zero!
+Inputting pressures over the selected maximum will reduce the value to the maximum. An inflation task "hug or profile" will only execute if the times are not zero!
 The values under all menus except CONFIG are usable for the current power cycle. The SAVE function will store the values for future use. Changing the CONFIG menu
-items require a reset to take effect. This is forcued in the CONFIG navigation.
+items requires a reset to take effect. This is forced in the CONFIG navigation.
 
 Operation notes:
 The noise level of the pump can be tuned by changing the maximum PWM in the AIRSYS menu. The threshold ramping value can also be adjusted to allow for a softer start/stop
-that can make the pump quieter during topping maintance.
-Motor pulses for system feedback. (One) pulse for system functions such as save function or failure to set a variable. (Two) pulses single the end of an inflation task.
+that can make the pump quieter during topping maintenance.
+Motor pulses for system feedback. (One) pulse for system functions such as the save function or failure to set a variable. (Two) pulses single the end of an inflation task.
 */
 
 //PINS
